d7645f2f0bbd67a0b8884a4bdeaf970607f7e553
kiyj12 <kiyj12@gmail.com>
2025-03-12 15:16:21 -0400

    boj 10818 solved

---
+++ boj/10818.py
@@ -0,0 +1,13 @@
+num_cnt = int(input())
+num_arr = list(map(int, input().split()))
+
+max_num = num_arr[0]
+min_num = num_arr[0]
+
+for i in range(1,num_cnt):
+    if max_num < num_arr[i]:
+        max_num = num_arr[i]
+    if min_num > num_arr[i]:
+        min_num = num_arr[i]
+
+print(min_num, max_num)
